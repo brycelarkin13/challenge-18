33a36e320045d3088f00a349df586001fe447c51
bryce <blarkin13@gmail.com>
2022-04-29 14:13:59 -0500

    routes added

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,33 @@
 const { User } = require('../models');
 
 const userController = {
-    
-}
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .then(dbUserData => {
+            // If no user found, send 404
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+};
+
+module.exports = userController;
