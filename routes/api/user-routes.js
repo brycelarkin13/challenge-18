471f0743d4df022fa12832e4667f786ea113fcb8
bryce <blarkin13@gmail.com>
2022-04-29 13:23:51 -0500

    user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,6 +5,30 @@ const {
     updateUser,
     deleteUser,
     getAllUsers,
+    getUserById,
     addToFriendsList,
     removeFromFriendsList
-} = require('../../controllers/')
+} = require('../../controllers/');
+
+// GET and POST routes api/user
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// GET one, Put, and Delete api/users/<id>    
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addToFriendsList);
+
+router
+    .route('/:userId/friends/:friendId')
+    .delete(removeFromFriendsList);
+
+module.exports = router;
